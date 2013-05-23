91cb25cadc3231b10cabfae58fda4cf2bc1db614
Liam O'Gorman <liamog@google.com>
2013-05-22 18:16:21 -0700

    This fixes the issue where when I save the file it only updates the image with the visible text. I generally want it to generate the entire image on save. So added a new command for the full file.

--- WebSequenceDiagrams.py
+++ WebSequenceDiagrams.py
@@ -33,20 +33,33 @@ class WebsequencediagramsListener(sublime_plugin.EventListener):
     def on_post_save(self, view):
         if view.file_name().endswith(('.wsd')):
             temp_file = getTempPreviewPath(view)
+            #if we save the file then we want to produce an image for the whole file.		
             if os.path.isfile(temp_file):
-                view.run_command('websequencediagrams')
+                view.run_command('websequencediagrams', {"whole_buffer": True, "preview":False})
 
 class WebsequencediagramsCommand(sublime_plugin.TextCommand):
-	def run(self, edit, target = 'browser'):
-		region = self.view.visible_region()
-		contents = self.view.substr(region)
+	def run(self, edit, whole_buffer=False, preview=True, target = 'browser'):
+		view = self.view
+		
+		contents = ''
+		if whole_buffer:
+			print "posting whole buffer"
+			for region in [sublime.Region(0, view.size())]:
+				contents += view.substr(region)
+		else :
+			print "posting just visible"
+			region = view.visible_region()
+			contents = view.substr(region)
+
 		if not "participant" in contents:
-			region = self.view.find('(participant .*\n)+', 0)
-			participants = self.view.substr(region)
+			region = view.find('(participant .*\n)+', 0)
+			participants = view.substr(region)
 			contents = participants + contents
 		style = "qsd"
 		pngFile = getTempPreviewPath(self.view)
-		if getSequenceDiagram(contents, pngFile, style):
+		if getSequenceDiagram(contents, pngFile, style) and preview:
+			print("Opening " + pngFile)
 			desktop.open(pngFile)
+
 
 
